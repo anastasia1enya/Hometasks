8a078ace3593cc356222909cbc75960d5400fe53
Anastasia Plaksienko <anastasia.enya@gmail.com>
2020-11-11 23:08:13 +0200

    о

--- HillelHometasks/src/com/Lesson2/AverageNumberOfN.java
+++ HillelHometasks/src/com/Lesson2/AverageNumberOfN.java
@@ -1,17 +1,24 @@
 package com.Lesson2;
 
+import java.util.Arrays;
+
 public class AverageNumberOfN {
     public static void main(String[] args) {
-        int[] numbers = { 4,5,11};
+        int[] Numbers = {5, 6, 7, 89, 10};
+        double sum = 0;
+        // рассчитываемд сумму всех чисел
 
-        int sum =0; // вводим изначальное значение
-        for (int i = 0; i <numbers.length; i++) {
-             sum += numbers[i];
+        for (int number : Numbers) {
+
+            sum += number;
+
         }
-        double average = sum/numbers.length;
-        System.out.println("The average number of " + average);
-        //System.out.println(numbers.toString());
+        // рассчитываем среднее арифметическое
+        double average = sum / Numbers.length;
+
+        System.out.println("The average for numbers:" + Arrays.toString(Numbers) + " is " + average);
 
     }
 
 }
+

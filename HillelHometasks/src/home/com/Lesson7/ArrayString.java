d9b5fa65f96a027f5f5a78a77792c40296dab966
anastasia1enya <anastasia.enya@gmail.com>
2020-11-30 16:11:55 +0200

    HW7

--- HillelHometasks/src/home/com/Lesson7/ArrayString.java
+++ HillelHometasks/src/home/com/Lesson7/ArrayString.java
@@ -1,20 +1,32 @@
 package home.com.Lesson7;
 
-
 import java.util.Arrays;
 
 public class ArrayString extends Methods {
 
     public static void main(String[] args) {
+        //создаем массив
 
-        String array [] = {"a","b","c","d"};
+        System.out.println("Original list:");
+
+        String countries [] = {"USA","Canada","Paris","Bulgari","Odessa", "Portugal", "Spain", "Germany"};
+
+        System.out.println(Arrays.toString(countries));
+      // создаем экземпляр класса
      Methods methods = new Methods();
 
-     /*String[] array1 = methods.addIndex(array,3,"BOOOOOO");
-     String[] array2 = methods.deleteIndex(array1,3);
-     methods.getIndex(array2,"d");*/
+        System.out.println("Modified one:");
+        System.out.println("___________________");
+     //добавляем/удаляем элементы
+     String [] countries1 = methods.deleteIndex(countries,2);
+     String [] countries2 = methods.deleteName(countries1,"Odessa");
+     String [] countries3 = methods.addIndex(countries2, 2,"France");
+     String [] countries4 = methods.addName(countries3 ,"Georgia");
+     String [] countries5 = methods.addName(countries4 ,"Brazil");
+     String [] countries6 = methods.addName(countries5 ,"Ecuador");
+     methods.getIndex(countries6,"Georgia");
 
-        methods.addName(array,"Gos");
+
 
 
 
--- HillelHometasks/src/home/com/Lesson7/Methods.java
+++ HillelHometasks/src/home/com/Lesson7/Methods.java
@@ -5,15 +5,16 @@ import java.util.Arrays;
 public class Methods {
 
     // метод добавления по индексу
-    public String [] addIndex (String [] array, int index, String name){
-        String arrayNew [] = Arrays.copyOfRange(array,0,array.length+1);
+    public String[] addIndex(String[] array, int index, String name) {
+        String arrayNew[] = Arrays.copyOfRange(array, 0, array.length + 1); //добавляем новый массив с длинной +1
 
-        int j=0;
-        for (int i = 0; i < array.length ; i++) {
-            if (i == index){
-                arrayNew [j++] = name;
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i == index) {
+                arrayNew[j++] = name;
 
-            } arrayNew [j++] = array[i];
+            }
+            arrayNew[j++] = array[i];
         }
         System.out.println(Arrays.toString(arrayNew));
         return arrayNew;
@@ -23,59 +24,63 @@ public class Methods {
 
     // метод добавления по значению
 
-    public void addName ( String [] array, String name){
-        String arrayNew [] = Arrays.copyOfRange(array,0,array.length+1);
-        int j =0;
-        for (int i = 0; i <= array.length ; i++) {
-            if (i == array.length){
-                arrayNew [j] = name;
-
+    public String[] addName(String[] array, String name) {
+        String arrayNew[] = Arrays.copyOfRange(array, 0, array.length + 1);//добавляем новый массив с длинной +1
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i <= array.length) {
+                arrayNew[j++] = array[i];
             }
-            arrayNew [j++] = array[i];
+            arrayNew[j] = name;
 
         }
         System.out.println(Arrays.toString(arrayNew));
+        return arrayNew;
     }
 
     // метод удаления по индексу
 
-    public String[]  deleteIndex(String [] array, int index){
-        String arrayNew [] = Arrays.copyOfRange(array,0,array.length-1);
+    public String[] deleteIndex(String[] array, int index) {
+        String arrayNew[] = Arrays.copyOfRange(array, 0, array.length - 1);//добавляем новый массив с длинной -1
 
-        int j=0;
-        for (int i = 0; i < array.length ; i++) {
-            if (i == index){
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i == index) {
                 i++;
 
-            } arrayNew [j++] = array[i];
-        }
-        System.out.println(Arrays.toString(arrayNew));
-        return arrayNew;
-
-    }
-    // метод удаления по значению
-    public String[]  deleteName (String [] array, String name){
-
-        String arrayNew [] = Arrays.copyOfRange(array,0,array.length-1);
-        int j=0;
-        for (int i = 0; i < array.length ; i++) {
-
-            if (array[i].equals(name)){
-                i++;
             }
-            arrayNew [j++] = array[i];
+            arrayNew[j++] = array[i];
         }
-
         System.out.println(Arrays.toString(arrayNew));
         return arrayNew;
 
     }
 
-    public void getIndex(String [] array, String name) {
+    // метод удаления по значению
+    public String[] deleteName(String[] array, String name) {
+
+        String arrayNew[] = Arrays.copyOfRange(array, 0, array.length - 1);//добавляем новый массив с длинной -1
+        int j = 0;
         for (int i = 0; i < array.length; i++) {
 
             if (array[i].equals(name)) {
-                System.out.println(i);
+                i++;
+            }
+            arrayNew[j++] = array[i];
+        }
+
+        System.out.println(Arrays.toString(arrayNew));
+        return arrayNew;
+
+    }
+
+    //метод получения индекса по значению
+
+    public void getIndex(String[] array, String name) {
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i].equals(name)) {
+                System.out.println("The index of element " + array[i] + " is [" + i + "]");
             }
 
         }

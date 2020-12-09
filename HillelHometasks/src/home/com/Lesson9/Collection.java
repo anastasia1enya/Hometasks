eae79deb551fa543591bd6b68dee2c9d7cc6ceed
Anastasia Plaksienko <anastasia.enya@gmail.com>
2020-12-09 22:51:02 +0200

    ls9

--- HillelHometasks/src/home/com/Lesson9/Collection.java
+++ HillelHometasks/src/home/com/Lesson9/Collection.java
@@ -2,68 +2,72 @@ package home.com.Lesson9;
 
 import java.util.Arrays;
 
-
 class Node {
-    private String [] next ;
-    private String [] prev ;
 
-    public String [] getNext() {
+    private String  next ;
+    private String prev ;
+    private String element ;
+
+    public String  getNext() {
         return  next;
     }
 
-    public void setNext(String []next) {
+    public void setNext(String next) {
         this.next = next;
     }
 
-    public String [] getPrev() {
+    public String  getPrev() {
         return prev;
     }
 
-    public void setPrev(String []prev) {
+    public void setPrev(String prev) {
         this.prev = prev;
     }
 
-    Node(String [] prev,String str, String [] next){
+    public String getElement() {
+        return element;
+    }
 
+    public void setElement(String element) {
+        this.element = element;
+    }
+
+    private String [] nodeArray;
+
+    public String[] getNodeArray() {
+        return nodeArray;
+    }
+
+    public void setNodeArray(String[] nodeArray) {
+        this.nodeArray = nodeArray;
+    }
+
+    Node(){
+
+        String [] nodeArray = {prev,element,next};
 
     }
 
-    public String [] next (String [] array) {
-
-        this.next = next;
+    public String [] info (String [] array) {
 
         for (int i = 0; i < array.length; i++) {
+            if (i==0){
+                prev = null;
+            } else { prev = array[i-1];}
+            if (i==array.length-1){
+                next = null;
+            } else { next = array[i+1];}
 
-            if (i == array.length-1 ) {
-                next [i] = null;
-            } else {
-                next [i] = array[i + 1];
-            }
-
+            element= array[i];
         }
-        return next;
+        return nodeArray;
     }
 
-    public String [] prev (String [] array) {
-
-        this.prev = prev;
-
-        for (int i = array.length-1; i >=1 ; i--) {
-
-            if (i == 1) {
-                prev [i] = null;
-            } else {
-                prev [i]= array[i - 1];
-            }
-
-        }
-        return prev;
-    }
 
 }
 
 
-public class Collection implements CustomCollection {
+public class Collection extends Node implements CustomCollection {
 
 
     public String getStr() {
@@ -96,34 +100,10 @@ public class Collection implements CustomCollection {
     private String str;
     private int count;
 
-    private String [] next1;
-    private String [] prev1;
-
-    public String []getNext1() {
-        return next1;
-    }
-
-    public void setNext1(String [] next1) {
-        this.next1 = next1;
-    }
-
-    public String [] getPrev1() {
-        return prev1;
-    }
-
-    public void setPrev1(String []prev1) {
-        this.prev1 = prev1;
-    }
-
-
 
 
     Collection (){
         array = new String[1];
-       Node node = new Node(prev1,str,next1);
-       // this.str = str;
-        next1=  node.next(array);
-        prev1 = node.prev(array);
 
     }
     public String toString() {
@@ -143,13 +123,16 @@ public class Collection implements CustomCollection {
             String[] arrayNew = Arrays.copyOfRange(array, 0, array.length + 1);
             for (int i = array.length - 1; i > count; i--) {
                 arrayNew[i + 1] = arrayNew[i];
+                Node node = new Node();
+                node.info(array);
+
+                System.out.println(Arrays.toString(getNodeArray()));
             }
             array = arrayNew;
 
         }
         array[count]=str;
-        System.out.println(next1[count]);
-        System.out.println(prev1[count]);
+
 
         count++;
 

f4ebf4fb52b1e37b4826267f7030a18aa1d63ad3
anastasia1enya <anastasia.enya@gmail.com>
2021-01-18 15:41:16 +0200

    15

---
+++ HillelHometasks/MavenHW/src/main/java/HW15/Converter.java
@@ -0,0 +1,74 @@
+package HW15;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.google.gson.Gson;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Converter {
+
+
+    String textDirectory = FileSystems.getDefault()
+            .getPath("")
+            .toAbsolutePath()
+            .toString()
+            +"\\src\\main\\java\\HW15\\results.txt";
+
+    //метод определения расширения файла
+    public  String getFileExtension(File file) {
+        String fileName = file.getName();
+        // если в имени файла есть точка и она не является первым символом в названии файла
+        if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+            // то вырезаем все знаки после последней точки в названии файла, то есть ХХХХХ.txt -> txt
+            return fileName.substring(fileName.lastIndexOf(".") +1);
+            // в противном случае возвращаем заглушку, то есть расширение не найдено
+        else return "";
+    }
+    //
+    public  void type(File file, String path) throws IOException {
+// from JSON to YML
+        if (getFileExtension(file).equals("json")) {
+            String json = ReadFromFile.readToString(path);
+            Yaml yaml = new Yaml();
+            Gson gson = new Gson();
+
+            Map map = gson.fromJson(json, Map.class);
+
+            System.out.println(map);
+            String outputYaml = yaml.dump(map);
+            System.out.println(outputYaml);
+
+// from  YML to JSON
+        } else if (getFileExtension(file).equals("yaml")) {
+
+            String yml = ReadFromFile.readToString(path);
+            Yaml yaml = new Yaml();
+            Gson gson = new Gson();
+
+            Map<String, Object> obj = (Map<String, Object>) yaml.load(yml);
+            String outputYaml = yaml.dump(obj);
+            System.out.println(outputYaml);
+
+            ObjectMapper yamlReader = new ObjectMapper(new YAMLFactory());
+            Object obj1 = yamlReader.readValue(yml, Object.class);
+
+
+            ObjectMapper jsonWriter = new ObjectMapper();
+            String outputJson = jsonWriter.writeValueAsString(obj1);
+
+            Map map = gson.fromJson(outputJson, Map.class);
+
+
+            System.out.println(map);
+
+        }
+
+
+    }
+}
--- HillelHometasks/MavenHW/src/main/java/HW15/Main.java
+++ HillelHometasks/MavenHW/src/main/java/HW15/Main.java
@@ -1,76 +1,55 @@
 package HW15;
 
-import com.google.gson.Gson;
-import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
-import java.io.InputStream;
-import java.util.Map;
-import java.util.Set;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+
 
 public class Main {
 
-    public static void main(String[] args) {
-        File file = new File("\\src\\main\\java\\HW15\\Files\\20_item.json");
-        System.out.println("Расширение файла: " + getFileExtension(file));
+    public static void main(String[] args) throws IOException {
 
-type(file);
+        Converter converter = new Converter();
+
+        String textDirectory1 = FileSystems.getDefault()
+                .getPath("")
+                .toAbsolutePath()
+                .toString()
+                +"\\src\\main\\java\\HW15\\Files\\20_item.json";
+        String textDirectory2 = FileSystems.getDefault()
+                .getPath("")
+                .toAbsolutePath()
+                .toString()
+                +"\\src\\main\\java\\HW15\\Files\\100_item.json";
+        String textDirectory3 = FileSystems.getDefault()
+                .getPath("")
+                .toAbsolutePath()
+                .toString()
+                +"\\src\\main\\java\\HW15\\Files\\test1.yaml";
+        String textDirectory4 = FileSystems.getDefault()
+                .getPath("")
+                .toAbsolutePath()
+                .toString()
+                +"\\src\\main\\java\\HW15\\Files\\test2.yaml";
+
+
+        File file1 = new File(textDirectory1);
+      //  System.out.println("Расширение файла: " + converter.getFileExtension(file1));
+        File file2 = new File(textDirectory2);
+       // System.out.println("Расширение файла: " +converter.getFileExtension(file2));
+        File file3 = new File(textDirectory3);
+      //  System.out.println("Расширение файла: " + converter.getFileExtension(file3));
+        File file4 = new File(textDirectory4);
+      //  System.out.println("Расширение файла: " +converter.getFileExtension(file4));
+
+        converter.type(file1,textDirectory1);
+        converter.type(file1,textDirectory2);
+        converter.type(file3,textDirectory3);
+        converter.type(file4,textDirectory4);
+
+    }
 
     }
 
-    //метод определения расширения файла
-    private static String getFileExtension(File file) {
-        String fileName = file.getName();
-        // если в имени файла есть точка и она не является первым символом в названии файла
-        if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
-            // то вырезаем все знаки после последней точки в названии файла, то есть ХХХХХ.txt -> txt
-            return fileName.substring(fileName.lastIndexOf(".") +1);
-            // в противном случае возвращаем заглушку, то есть расширение не найдено
-        else return "";
-    }
 
-    public static void type(File file) {
-
-       // File f = new File("\\src\\main\\java\\HW15\\test2.yaml");
-if (getFileExtension(file).equals("json")){
-    String json = ReadFromFile.readToString("C:\\Users\\Yuliia\\Desktop\\Hometasks\\HillelHometasks\\MavenHW\\src\\main\\java\\HW15\\Files\\20_item.json");
-
-    Gson gson = new Gson();
-    Map map = gson.fromJson(json, Map.class);
-
-    System.out.println(map);
-
-    Set<String> set = map.keySet();
-
-    for (String str : set) {
-        System.out.println("-----------------------");
-        System.out.print(str + " ... ");
-        //System.out.println(map.get(str));
-      //  System.out.println(map.get(str).getClass());
-
-    }
-
-
-    Yaml yaml = new Yaml();
-  //  String yamlStr = ReadFromFile.readToString("C:\\Users\\Yuliia\\Desktop\\Hometasks\\HillelHometasks\\MavenHW\\src\\main\\java\\HW15\\Files\\20_item.json");
- //   Map<String, Object> obj = yaml.load((InputStream) set);
-   // System.out.println(obj);
-
-
-    Set<String> set1 = map.keySet();
-
-    for (String str : set1) {
-        System.out.println("-----------------------");
-        System.out.print(str + " ... ");
-        //System.out.println(map.get(str));
-        //  System.out.println(map.get(str).getClass());
-
-    }
-    yaml.dump(set1);
-
-}else{
-    System.out.println("6yaml");
-}
-
-    }
-}

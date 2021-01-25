6e4ea88d50607e12fc79249b35a220d0a815714b
anastasia1enya <anastasia.enya@gmail.com>
2021-01-25 17:27:58 +0200

    15

--- HillelHometasks/MavenHW/src/main/java/HW15/Main.java
+++ HillelHometasks/MavenHW/src/main/java/HW15/Main.java
@@ -15,11 +15,18 @@ public class Main {
                 .getPath("")
                 .toAbsolutePath()
                 .toString());
+
         File textDirectory =new File( FileSystems.getDefault()
                 .getPath("")
                 .toAbsolutePath()
                 .toString()
                 + "\\src\\main\\java\\HW15\\Files");
+
+        if (args.length!=0){
+            workDir = new File(args[0]);
+        } else {
+            workDir = new File (String.valueOf(textDirectory));
+        }
 
 //        System.out.println(workDir);
 //        System.out.println(textDirectory);
@@ -27,57 +34,23 @@ public class Main {
 int i = 0;
 
         String a = textDirectory + "\\src\\main\\java\\HW15\\Files";
-        for (File f :workDir.listFiles()){
-        if (f.isFile()){
 
-            System.out.println("-----"+i++);
-    converter.type(f,f.getAbsolutePath());
-    System.out.println(f.getAbsolutePath());
-}
-   else {
-            converter.type(f, a);
-            System.out.println("////////" + i++);
-            System.out.println(f.getPath());
-            System.out.println(a);
-        }
-        }
+            for (File f : workDir.listFiles()) {
 
+                    if (f.isFile()) {
 
-//        String textDirectory1 = FileSystems.getDefault()
-//                .getPath("")
-//                .toAbsolutePath()
-//                .toString()
-//                + "\\src\\main\\java\\HW15\\Files\\20_item.json";
-//        String textDirectory2 = FileSystems.getDefault()
-//                .getPath("")
-//                .toAbsolutePath()
-//                .toString()
-//                + "\\src\\main\\java\\HW15\\Files\\100_item.json";
-//        String textDirectory3 = FileSystems.getDefault()
-//                .getPath("")
-//                .toAbsolutePath()
-//                .toString()
-//                + "\\src\\main\\java\\HW15\\Files\\test1.yaml";
-//        String textDirectory4 = FileSystems.getDefault()
-//                .getPath("")
-//                .toAbsolutePath()
-//                .toString()
-//                + "\\src\\main\\java\\HW15\\Files\\test2.yaml";
+                    System.out.println("-----" + i++);
+                    converter.type(f, f.getAbsolutePath());
+                    System.out.println(f.getAbsolutePath());
+                } else {
 //
-//
-//        File file1 = new File(textDirectory1);
-////        System.out.println("Расширение файла: " + converter.getFileExtension(file1));
-//        File file2 = new File(textDirectory2);
-////        System.out.println("Расширение файла: " +converter.getFileExtension(file2));
-//        File file3 = new File(textDirectory3);
-////        System.out.println("Расширение файла: " + converter.getFileExtension(file3));
-//        File file4 = new File(textDirectory4);
-////        System.out.println("Расширение файла: " +converter.getFileExtension(file4));
-//
-//        converter.type(file1, textDirectory1);
-//        converter.getFileStructure(file2, textDirectory2);
-//        converter.getFileStructure(file3, textDirectory3);
-//        converter.getFileStructure(file4, textDirectory4);
+                    converter.type(f, a);
+                    System.out.println("////////" + i++);
+                    System.out.println(f.getPath());
+                    System.out.println(a);
+                }
+            }
+
 
     }
 }

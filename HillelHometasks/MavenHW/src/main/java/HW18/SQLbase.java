dbb8e2d7c4d738422cb976040534261f7092b775
anastasia1enya <anastasia.enya@gmail.com>
2021-02-02 18:22:20 +0200

    18

--- HillelHometasks/MavenHW/src/main/java/HW18/SQLbase.java
+++ HillelHometasks/MavenHW/src/main/java/HW18/SQLbase.java
@@ -1,14 +1,14 @@
 package HW18;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class SQLbase {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connect = DriverManager
                 .getConnection("jdbc:mysql://localhost: 3306/Students?"+"user=root&password=rootroot");
+        Statement statement = connect.createStatement();
+        ResultSet resultset = statement.executeQuery("select*from Students.Groups");
+        System.out.println(resultset.getMetaData());
     }
 }

1febb686596ef6b5408a5e119d6cf5b1ff11107e
anastasia1enya <anastasia.enya@gmail.com>
2021-02-08 16:48:27 +0200

    18

--- HillelHometasks/MavenHW/src/main/java/HW18/SQLbase.java
+++ HillelHometasks/MavenHW/src/main/java/HW18/SQLbase.java
@@ -8,10 +8,19 @@ public class SQLbase {
         Connection connect = DriverManager
                 .getConnection("jdbc:mysql://localhost: 3306/Students?"+"user=root&password=rootroot");
         Statement statement = connect.createStatement();
-        ResultSet resultset = statement.executeQuery("select*from Students.Groups");
+        ResultSet resultset = statement.executeQuery("select*from Students.Student");
         System.out.println(resultset.getMetaData());
 
-        PreparedStatement preparedStatement = connect.prepareStatement("insert into student " );
+        PreparedStatement preparedStatement = connect.prepareStatement("insert into students.student(name_surname) values (?)" );
+
+        try{
+            preparedStatement.setString(1,"Ivan Petrov");
+            preparedStatement.executeUpdate();
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
 
 
     }

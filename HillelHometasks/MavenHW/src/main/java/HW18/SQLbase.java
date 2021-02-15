7c3f593f5edb3377476bfd2d949df5c95e8c83e5
anastasia1enya <anastasia.enya@gmail.com>
2021-02-15 17:49:09 +0200

    18

--- HillelHometasks/MavenHW/src/main/java/HW18/SQLbase.java
+++ HillelHometasks/MavenHW/src/main/java/HW18/SQLbase.java
@@ -11,37 +11,74 @@ public class SQLbase {
         ResultSet resultset = statement.executeQuery("select*from Students.Student");
         System.out.println(resultset.getMetaData());
 
-        PreparedStatement preparedStatement = connect.prepareStatement("INSERT into Students.Student(name_surname, `group`,yearOfEntering ) values (?,?,?)" );
-//        PreparedStatement preparedStatement = connect.prepareStatement("INSERT into Students.Groups(idGroups,group) values (?,?)" );
+//        PreparedStatement preparedStatement = connect.prepareStatement("INSERT into Students.Student(name_surname, `group`,yearOfEntering ) values (?,?,?)" );
+//
+//        try{
+//
+//            preparedStatement.setString(1,"Johanson Steam");
+//            preparedStatement.setInt(2,1);
+//            preparedStatement.setInt(3,2020);
+//           preparedStatement.executeUpdate();
+//
+//            ResultSet result2 = preparedStatement.executeQuery("select*from Students.Student");
+//
+//
+//            System.out.println("Выводим PreparedStatement");
+//            while (result2.next()) {
+//                System.out.println("Номер в выборке #" + result2.getRow()
+//                        + "\t Номер в базе #" + result2.getInt("studId")
+//                        + "\t" + result2.getString("name_surname")
+//                        + "\t" + result2.getString("group")
+//                        + "\t" + result2.getInt("yearOfEntering"));
+//            }
+//
+//        }catch (SQLException e){
+//            e.printStackTrace();
+//        }
+
+        PreparedStatement preparedStatement1 = connect.prepareStatement("SELECT \n" +
+                "    Student\n" +
+                "FROM\n" +
+                "    referential_constraints\n" +
+                "WHERE\n" +
+                "    constraint_schema = 'Students'\n" +
+                "        AND referenced_table_name = 'group'\n" +
+                "        AND delete_rule = 'CASCADE'" );
 
         try{
-//            preparedStatement.setInt(1,100);
-            preparedStatement.setString(1,"Johanson Steam");
-            preparedStatement.setInt(2,1);
-            preparedStatement.setInt(3,2020);
+////          preparedStatement.setInt(1,100);
+//            preparedStatement1.setString(1,"Johanson Steam");
+//            preparedStatement1.setInt(2,1);
+//            preparedStatement1.setInt(3,2020);
 //            preparedStatement.setString(1,"Petrov Ivan");
 //            preparedStatement.setString(1,"3");
 //            preparedStatement.setString(1,"2015");
-           preparedStatement.executeUpdate();
 
-            ResultSet result2 = preparedStatement.executeQuery("select*from Students.Student");
+
+            ResultSet result3 = preparedStatement1.executeQuery("\"SELECT \\n\" +\n" +
+                    "                \"    Student\\n\" +\n" +
+                    "                \"FROM\\n\" +\n" +
+                    "                \"    referential_constraints\\n\" +\n" +
+                    "                \"WHERE\\n\" +\n" +
+                    "                \"    constraint_schema = 'Students'\\n\" +\n" +
+                    "                \"        AND referenced_table_name = 'group'\\n\" +\n" +
+                    "                \"        AND delete_rule = 'CASCADE'\" ");
+//            preparedStatement1.executeUpdate();
 
 //            System.out.println(result2.getMetaData());
 
             System.out.println("Выводим PreparedStatement");
-            while (result2.next()) {
-                System.out.println("Номер в выборке #" + result2.getRow()
-                        + "\t Номер в базе #" + result2.getInt("studId")
-                        + "\t" + result2.getString("name_surname")
-                        + "\t" + result2.getString("group")
-                        + "\t" + result2.getInt("yearOfEntering"));
+            while (result3.next()) {
+                System.out.println("Номер в выборке #" + result3.getRow()
+                        + "\t Номер в базе #" + result3.getInt("studId")
+                        + "\t" + result3.getString("name_surname")
+                        + "\t" + result3.getString("group")
+                        + "\t" + result3.getInt("yearOfEntering"));
             }
 
         }catch (SQLException e){
             e.printStackTrace();
         }
-
-
 
     }
 }

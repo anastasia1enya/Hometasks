42ef45f396805aabbc2b58cd9170c12b6c7b17a5
anastasia1enya <anastasia.enya@gmail.com>
2021-02-22 18:06:58 +0200

    18

--- HillelHometasks/MavenHW/src/main/java/HibernateHW/HibernateUtil.java
+++ HillelHometasks/MavenHW/src/main/java/HibernateHW/HibernateUtil.java
@@ -21,20 +21,6 @@ public class HibernateUtil {
 
     public static SessionFactory getSessionFactory() {
 
-//        if (sessionFactory == null) {
-//            try {
-//                Configuration configuration = new Configuration().configure();
-//                configuration.addAnnotatedClass(Student.class);
-//                configuration.addAnnotatedClass(Groups.class);
-//                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
-//                sessionFactory = configuration.buildSessionFactory(builder.build());
-//
-//            } catch (Exception e) {
-//                System.out.println("Исключение!" + e);
-//            }
-//        }
-
-
         return sessionFactory;
     }
 
--- HillelHometasks/MavenHW/src/main/java/HibernateHW/Main.java
+++ HillelHometasks/MavenHW/src/main/java/HibernateHW/Main.java
@@ -7,46 +7,47 @@ public class Main {
 
 Creation d = new Creation();
 
-d.recordsFindName("Sam Wintchester");
+//d.recordsFindName("Sam Wintchester");
 
-//        System.out.println("Hello! I'm smart system!What you would like to do?");
-//        System.out.println("1-create\n"+"2-find by name\n"+"3-find by id\n");
-//
-//        Scanner scanner = new Scanner(System.in);
-//
-//        int choice = scanner.nextInt();
-//
-//        switch (choice) {
-//            case 1:
-//                System.out.println("Pls enter the student's name&surname");
-//
-//                String a = scanner.next();
-//
-//
-//                System.out.println("Pls enter the group");
-//                int b = scanner.nextInt();
-//
-//                System.out.println("Pls enter year");
-//
-//                int c = scanner.nextInt();
-//                d.recordsAdd(a,b,c);
-//            break;
-//
-//            case 2:
-//                System.out.println("Pls enter the student's name&surname");
-//                String e = scanner.next();
-//                d.recordsFindName(e);
-//                break;
-//            case 3:
-//                System.out.println("Pls enter the student's id");
-//                int f = scanner.nextInt();
-//                d.recordsFindId(f);
-//                break;
-//
-//            default:
-//                System.out.println("Incorrect");
-//                break;
-//        }
+        System.out.println("Hello! I'm smart system!What you would like to do?");
+        System.out.println("1-create\n"+"2-find by name\n"+"3-find by id\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Pls enter the student's name&surname");
+
+                String a = scanner.next();
+
+
+                System.out.println("Pls enter the group");
+                int b = scanner.nextInt();
+
+                System.out.println("Pls enter year");
+
+                int c = scanner.nextInt();
+                d.recordsAdd(a,b,c);
+            break;
+
+            case 2:
+                System.out.println("Pls enter the student's name&surname");
+                String e = scanner.next();
+
+                d.recordsFindName(e);
+                break;
+            case 3:
+                System.out.println("Pls enter the student's id");
+                int f = scanner.nextInt();
+                d.recordsFindId(f);
+                break;
+
+            default:
+                System.out.println("Incorrect");
+                break;
+        }
 
     }
 }

00fd0118f0b85630b5cd4ace0d1fa325e6c53785
anastasia1enya <anastasia.enya@gmail.com>
2021-03-11 18:19:11 +0200

    15

--- HillelHometasks/MavenHW/src/main/java/CacheHW/User.java
+++ HillelHometasks/MavenHW/src/main/java/CacheHW/User.java
@@ -16,4 +16,12 @@ public class User {
     private String phone;
     private String email;
 
+//    public User(int id, String firstName, String lastName, int age, String phone, String email) {
+//        this.id = id;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.age =age;
+//        this.phone = phone;
+//        this.email = email;
+//    }
 }

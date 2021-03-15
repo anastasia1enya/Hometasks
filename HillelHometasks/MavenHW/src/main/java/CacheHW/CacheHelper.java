927cd326131f5c7bc222b0ca13fa18132e3da916
anastasia1enya <anastasia.enya@gmail.com>
2021-03-15 17:10:56 +0200

    15

--- HillelHometasks/MavenHW/src/main/java/CacheHW/CacheHelper.java
+++ HillelHometasks/MavenHW/src/main/java/CacheHW/CacheHelper.java
@@ -1,75 +1,63 @@
 package CacheHW;
 
-import CacheClass.User;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import org.ehcache.Cache;
-import org.ehcache.CacheManager;
-import org.ehcache.config.builders.CacheConfigurationBuilder;
-import org.ehcache.config.builders.CacheManagerBuilder;
-import org.ehcache.config.builders.ResourcePoolsBuilder;
-import org.ehcache.expiry.Duration;
-import org.ehcache.expiry.Expirations;
-
 import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 public class CacheHelper {
 
-    private  HashMap<String, HashMap<String, Object>> userCache;
+    private HashMap<String, HashMap<String, Object>> userCache;
 
-    CacheHelper(){
-        HashMap<String, Object> userCache = new HashMap<>();
+    private HashMap<String, Object> userCache1;
+
+    CacheHelper() {
+        userCache = new HashMap<>();
+        userCache1 = new HashMap<>();
     }
-
 
 
     public boolean putCache(String cacheName, String key, Object o) throws InterruptedException {
 
-
-        HashMap<String, Object> userCache1 = new HashMap<>();
-
-        userCache1.put(key,o);
-
         if (userCache.containsKey(cacheName)) {
-            userCache.put( cacheName,userCache1);
+            userCache1.put(key, o);
+            userCache.put(cacheName, userCache1);
             System.out.println("sucses");
             return true;
         } else {
-            System.out.println("false");
-            return false;
+            userCache1 = new HashMap<>();
+            userCache1.put(key, o);
+            userCache.put(cacheName, userCache1);
+            System.out.println("sucses and create");
+
+            return true;
 
         }
     }
 
-        public Object getCache(String cacheName, String key) throws InterruptedException {
+    @Override
+    public String toString() {
+        return
+                userCache.toString()
+                ;
+    }
 
-            if (userCache.containsKey(cacheName) && userCache.containsValue(key)) {
-                System.out.println("get value from cache");
-                return (Object) userCache.get(key);
-            } else {
+    public Object getCache(String cacheName, String key) throws InterruptedException {
 
-                return String.format("value not found in cache with id : %s", key);
 
-            }
 
-//    public Cache<String, Object> getUserCache() {
-//        return cacheManager.getCache(name, String.class, Object.class);
-//    }
-//
-//    public void clearCache(){
-//        cacheManager.getCache(name, String.class, Object.class).clear();
-//    }
+        if (userCache.containsKey(cacheName) && userCache1.containsKey(key)) {
+            System.out.println("get value from cache");
+            return (Object) userCache1.get(key);
+        } else {
 
-//    public CacheHelper(String name) {
-//
-//        this.name = name;
-//
-//
-//        userCache.put(name,new HashMap<String,Object>());
-//    }
+            return String.format("value not found in cache with id : %s", key);
 
+        }
+    }
+
+    public Object getCache1(String cacheName, String key) throws InterruptedException {
+
+        System.out.println(userCache1.get(cacheName));
+        return userCache1.get(cacheName);
+
+    }
 }
-}
+
--- HillelHometasks/MavenHW/src/main/java/CacheHW/Main.java
+++ HillelHometasks/MavenHW/src/main/java/CacheHW/Main.java
@@ -5,7 +5,12 @@ public class Main {
         CacheHelper ch = new CacheHelper();
 
         ch.putCache("names","1", "Petrov");
-        ch.getCache("names","1");
+        System.out.println(ch.toString());
+        ch.putCache("names","2", "Ivanov");
+        System.out.println(ch.toString());
+        ch.putCache("lol","3", "Andrew");
+        System.out.println(ch.toString());
+        System.out.println(ch.getCache1("names", "2"));
 
     }
 }
